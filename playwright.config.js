832ebf8696eb47c9773cea3b4266f56df327c3fd
Yury Kurouski <ymorpheus@gmail.com>
2025-11-10 15:56:31 +0100

    init commit

---
+++ playwright.config.js
@@ -0,0 +1,62 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+    testDir: './tests',
+
+    /* Maximum time one test can run for */
+    timeout: 30 * 1000,
+
+    /* Fail the build on CI if you accidentally left test.only in the source code */
+    forbidOnly: !!process.env.CI,
+
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+
+    /* Opt out of parallel tests on CI */
+    workers: process.env.CI ? 1 : undefined,
+
+    /* Reporter to use */
+    reporter: [
+        ['html', { outputFolder: 'playwright-report' }],
+        ['json', { outputFile: 'test-results/results.json' }],
+        ['list']
+    ],
+
+    /* Shared settings for all the projects below */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')` */
+        baseURL: 'https://demoqa.com',
+
+        /* Collect trace when retrying the failed test */
+        trace: 'on-first-retry',
+
+        /* Screenshot on failure */
+        screenshot: 'only-on-failure',
+
+        /* Video on failure */
+        video: 'retain-on-failure',
+    },
+
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome'],
+                viewport: { width: 1280, height: 720 },
+            },
+        },
+
+        {
+            name: 'firefox',
+            use: {
+                ...devices['Desktop Firefox'],
+                viewport: { width: 1280, height: 720 },
+            },
+        },
+    ],
+});
