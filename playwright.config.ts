dc3d856651032983093e7e8c0ba7e3b902822898
Yury Kurouski <ymorpheus@gmail.com>
2025-12-24 21:48:42 +0100

    chore: update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,12 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
+    fullyParallel: true,
     timeout: 30 * 1000,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.workers ? parseInt(process.env.workers) : (process.env.CI ? 1 : undefined),
+    grep: process.env.runThis ? new RegExp(process.env.runThis) : undefined,
     reporter: [
         ['html', { outputFolder: 'playwright-report' }],
         ['json', { outputFile: 'test-results/results.json' }],
@@ -15,20 +17,21 @@ export default defineConfig({
         baseURL: 'https://demoqa.com',
         trace: 'on-first-retry',
         screenshot: 'only-on-failure',
+        viewport: (process.env.VIEWPORT_WIDTH && process.env.VIEWPORT_HEIGHT)
+            ? { width: parseInt(process.env.VIEWPORT_WIDTH), height: parseInt(process.env.VIEWPORT_HEIGHT) }
+            : { width: 1280, height: 720 },
     },
     projects: [
         {
             name: 'chromium',
             use: {
                 ...devices['Desktop Chrome'],
-                viewport: { width: 1280, height: 720 },
             },
         },
         {
             name: 'firefox',
             use: {
                 ...devices['Desktop Firefox'],
-                viewport: { width: 1280, height: 720 },
             },
         },
     ],
